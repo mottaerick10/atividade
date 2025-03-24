94b7b7db8472154906556c360f4822ffd7ae880b
erick motta <mottaerick0000@gmail.com>
2025-03-24 20:53:34 -0300

    atividade

--- hello-world.js
+++ hello-world.js
@@ -1,6 +1,22 @@
 const nameInput = document.getElementById('name');
 const messageP = document.getElementById('message');
 
+const clearAll = () => {
+    nameInput.value = '';
+    messageP.innerText = '';
+}
+
+nameInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        helloWorld();
+    } else if (event.key === 'Escape') {
+        event.preventDefault();
+        clearAll();
+    }
+});
+
+
 nameInput.onkeyup = (event) => {
     if (nameInput.value === '') {
         messageP.innerText = '';
